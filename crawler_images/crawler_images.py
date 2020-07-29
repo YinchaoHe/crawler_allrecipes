0873665764c80cdd430cc1b57a254afa72ef732b
Yinchao He <yche@mail.google.com>
2020-07-29 02:04:30 -0500

    chromedriver

--- crawler_images/crawler_images.py
+++ crawler_images/crawler_images.py
@@ -22,7 +22,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", "--keywords", help="keywords for search, please no space", type=str, required=True)
     parser.add_argument("-l", "--limit", help="the amount of images", type=int, required=True)
-    parser.add_argument("-c", "--chromedriver", help="chromedriver", type=str, default="/Users/yinchaohe/Downloads/chromedriver", required=False)
+    parser.add_argument("-c", "--chromedriver", help="chromedriver", type=str, default="/home/yche/chromedriver", required=False)
     parser.add_argument("-d", "--image_directory", help="image_directory", type=str, default="rose", required=True)
     args = parser.parse_args()
     crawler_images(args)
