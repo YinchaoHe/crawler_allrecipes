b2a4cb3b94cf79f7f4d60d111b22676e5b386928
Yhe <yhe@huskers.unl.edu>
2020-10-04 18:40:04 -0400

    add USDA excel

--- crawler_allrecipes/USDA_excel.py
+++ crawler_allrecipes/USDA_excel.py
@@ -30,22 +30,24 @@ def main():
         with open('result/' + str(item) + '.json') as f:
             data = json.load(f)
         os.remove('result/' + str(item) + '.json')
-        result = {"ingredient": data["description"],
-                  "portion": '100g'}
+        try:
+            result = {"ingredient": data["description"],
+                      "portion": '100g'}
 
-        nu_info = {}
-        for nutrient_info in data["foodNutrients"]:
-            nutrient = nutrient_info['nutrient']
-            if nutrient['id'] == 1004 or nutrient['id'] == 1253 or nutrient['id'] ==  1093 or nutrient['id'] == 1092 or nutrient['id'] == 2039 or nutrient['id'] == 1079 or nutrient['id'] == 1003 or nutrient['id'] == 2000:
-                nu_info[nutrient['name']] = str(nutrient['rank'] / 100) +  nutrient['unitName']
-            if nutrient['id'] == 1106 or nutrient['id'] == 1162 or nutrient['id'] == 1087 or nutrient['id'] == 1089 or nutrient['id'] == 1165 or nutrient['id'] == 1167 or nutrient['id'] == 1175 or nutrient['id'] == 1090 or nutrient['id'] == 1187:
-                nu_info[nutrient['name']] = str(nutrient['rank'] / 100) + nutrient['unitName']
-        result['nutrition'] = nu_info
-        name = data["description"].replace(" ", "")
-        name = name.replace(",", "_")
-        with open('result/' + name + '.json', 'w') as f:
-            json.dump(result, f)
-
+            nu_info = {}
+            for nutrient_info in data["foodNutrients"]:
+                nutrient = nutrient_info['nutrient']
+                if nutrient['id'] == 1004 or nutrient['id'] == 1253 or nutrient['id'] ==  1093 or nutrient['id'] == 1092 or nutrient['id'] == 2039 or nutrient['id'] == 1079 or nutrient['id'] == 1003 or nutrient['id'] == 2000:
+                    nu_info[nutrient['name']] = str(nutrient['rank'] / 100) +  nutrient['unitName']
+                if nutrient['id'] == 1106 or nutrient['id'] == 1162 or nutrient['id'] == 1087 or nutrient['id'] == 1089 or nutrient['id'] == 1165 or nutrient['id'] == 1167 or nutrient['id'] == 1175 or nutrient['id'] == 1090 or nutrient['id'] == 1187:
+                    nu_info[nutrient['name']] = str(nutrient['rank'] / 100) + nutrient['unitName']
+            result['nutrition'] = nu_info
+            name = data["description"].replace(" ", "")
+            name = name.replace(",", "_")
+            with open('result/' + name + '.json', 'w') as f:
+                json.dump(result, f)
+        except:
+            continue
 
 
 
