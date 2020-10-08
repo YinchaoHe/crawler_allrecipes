d5fb5118037d1ee9b6ade0e22462a05e9d44a881
Yhe <yhe@huskers.unl.edu>
2020-10-08 15:03:25 -0400

    calo

--- crawler_allrecipes/crawler_calo.py
+++ crawler_allrecipes/crawler_calo.py
@@ -7,18 +7,17 @@ import urllib.request as req
 import bs4
 
 def main():
-
+    dir = 'calories_info'
+    try:
+        os.mkdir(dir)
+    except:
+        print('this folder exists')
     os.system("find . -name original_recipes_info > re_info_path.txt")
     with open("re_info_path.txt") as f:
         paths = f.readlines()
     for path in paths:
         output_path = path.replace('\n', '').split('original_recipes_info')[0]
         output_path = output_path.split('./')[1]
-        dir = output_path.split('/')[0] + '_calories_info'
-        try:
-            os.mkdir(dir)
-        except:
-            print('this folder exists')
         output_path = output_path.replace('/', '_')
         print(output_path)
         # output_path += 'calories_info'
