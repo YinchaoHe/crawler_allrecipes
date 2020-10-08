14bfcaa59c4582c7747ed302f4d25402eaf5d40a
Yhe <yhe@huskers.unl.edu>
2020-10-08 00:39:53 -0400

    calo

--- crawler_allrecipes/crawler_calo.py
+++ crawler_allrecipes/crawler_calo.py
@@ -7,16 +7,24 @@ import urllib.request as req
 import bs4
 
 def main():
+    dir = 'calories_info'
+    try:
+        os.mkdir(dir)
+    except:
+        print('this folder exists')
     os.system("find . -name original_recipes_info > re_info_path.txt")
     with open("re_info_path.txt") as f:
         paths = f.readlines()
     for path in paths:
         output_path = path.replace('\n', '').split('original_recipes_info')[0]
-        output_path += 'calories_info'
-        try:
-            os.mkdir(output_path)
-        except:
-            print("This folder exists")
+        output_path = output_path.split('./')[1]
+        output_path = output_path.replace('/', '_')
+        print(output_path)
+        # output_path += 'calories_info'
+        # try:
+        #     os.mkdir(output_path)
+        # except:
+        #     print("This folder exists")
         path = path.replace('\n', '') + '/'
         files=glob.glob(path + '*')
         calo_info = []
@@ -29,7 +37,7 @@ def main():
                     print(recipe['recipe_ID'])
                     calo = get_all_nutrition(recipe)
                     calo_info.append(calo)
-            with open(output_path + '/' +  str(index) + '_calories_info.json', 'w') as f:
+            with open(dir + '/' + output_path +  str(index) + '_calories_info.json', 'w') as f:
                 json.dump(calo_info, f)
             index += 1
     os.remove('re_info_path.txt')
